93d4aa3fd2fb84c28e58266b7ea860d509e34fcd
Yang.Yang <>
2012-12-16 19:46:52 -0500

    First commit

---
+++ hello.git/src/test/java/com/horizon2/hello/git/AppTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.horizon2.hello.git;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author yy
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() {
+         
+         assertTrue(true);
+         App.main(new String[]{});
+     
+     }
+}
